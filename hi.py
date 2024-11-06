7b87b166877b807533316fc36e32042c7c368a44
Archithaarav <kmitengineer5@gmail.com>
2024-11-06 12:59:49 +0530

    lost the previous commits

--- hi.py
+++ hi.py
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
 print('hello Aarav!!')
+<<<<<<< HEAD
 print("nice to meet you..")
 
 print("addin to branch1")
 =======
 print('hello Aarav!!')
 >>>>>>> parent of bb6e719 (3rd commit)
+=======
+print("nice to meet you..")
+>>>>>>> parent of cbb4e33 (adding to branch1)
