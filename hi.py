9abe028f2c6ca3bd7963ab071fc5bc1e7c044f04
Archithaarav <kmitengineer5@gmail.com>
2024-11-06 13:02:14 +0530

    after revert and new commit

--- hi.py
+++ hi.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
 print('hello Aarav!!')
-<<<<<<< HEAD
-print("nice to meet you..")
 
-print("addin to branch1")
-=======
-print('hello Aarav!!')
->>>>>>> parent of bb6e719 (3rd commit)
-=======
-print("nice to meet you..")
->>>>>>> parent of cbb4e33 (adding to branch1)
+print("after revert")
